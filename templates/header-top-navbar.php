a721199c0e609c226193fa8c83b5acae2b791341
Kalen Johnson <info@kalenjohnson.com>
2013-12-08 13:10:14 -0800

    Updated header with layout

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,21 +1,25 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
-    </div>
+<header class="banner container" role="banner">
+	<div class="top">
+		<a class="navbar-brand" href="<?php echo home_url(); ?>/"><h1><?php bloginfo( 'name' ); ?></h1></a>
+		<p class="description"><?php bloginfo( 'description' ); ?></p>
+		<div class="visible-lg">
+			<?php the_widget( 'WP_Widget_Search' ); ?>
+		</div>
+	</div>
+  <div class="navbar navbar-default navbar-static-top">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
 
     <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
+		<?php
+			if (has_nav_menu('primary_navigation')) :
+				wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+			endif;
+		?>
     </nav>
   </div>
 </header>
