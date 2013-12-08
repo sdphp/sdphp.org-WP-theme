22e8e2c61094205f377dcbcc3c1f94935db5b8fb
Kalen Johnson <info@kalenjohnson.com>
2013-12-08 14:47:33 -0800

    Updated CSS

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '739e5488f9753df1c0bdce368ab890e8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1853f251f0e208c51446337adb137797');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
