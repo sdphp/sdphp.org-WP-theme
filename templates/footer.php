73ee2f0ba301e05eec47eaa6ae4a37eb73cf314f
Kalen Johnson <info@kalenjohnson.com>
2013-12-08 14:47:40 -0800

    Updated Footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+      <p class="alignleft">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
   </div>
 </footer>
