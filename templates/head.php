a1adac76b9b2e3e2b16f07404c0332f033efc161
Kalen Johnson <info@kalenjohnson.com>
2013-12-08 13:09:57 -0800

    Added Source Sans Pro google font

--- templates/head.php
+++ templates/head.php
@@ -6,6 +6,8 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700,200italic,400italic,700italic' rel='stylesheet' type='text/css'>
+
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
